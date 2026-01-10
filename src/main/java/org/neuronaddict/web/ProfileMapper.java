6be49bdb6b5c4dc49563ec6379066148ee17a715
neuronaddict <22725031+neuronaddict@users.noreply.github.com>
2026-01-10 23:57:28 +0100

    Fix ProfileMapper

--- src/main/java/org/neuronaddict/web/ProfileMapper.java
+++ src/main/java/org/neuronaddict/web/ProfileMapper.java
@@ -1,9 +1,11 @@
 package org.neuronaddict.web;
 
 
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import org.neuronaddict.data.Profile;
 
+@Mapper
 public interface ProfileMapper {
 
     ProfileMapper INSTANCE = Mappers.getMapper(ProfileMapper.class);
