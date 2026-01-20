c39e4cfa09bf030bf3e1280960270a31b5c7c6ae
neuronaddict <22725031+neuronaddict@users.noreply.github.com>
2026-01-20 02:16:44 +0100

    Menu simplification

--- src/main/java/org/neuronaddict/web/NavExtensions.java
+++ src/main/java/org/neuronaddict/web/NavExtensions.java
@@ -2,18 +2,21 @@ package org.neuronaddict.web;
 
 import io.quarkus.qute.TemplateExtension;
 
+import java.util.Objects;
+
 public class NavExtensions {
 
-    @TemplateExtension(namespace = "nav")
-    public static String active(String path, String currentPath) {
-        if (path != null && path.equals(currentPath)) {
-             return "bg-indigo-600 text-white font-medium";
-        }
-        return "hover:bg-slate-800 hover:text-slate-200 transition";
-    }
+    public static final String BUTTON_STYLE_ACTIVE = "bg-indigo-600 text-white font-medium";
+    public static final String BUTTON_STYLE_INACTIVE = "hover:bg-slate-800 hover:text-slate-200 transition";
 
     @TemplateExtension(namespace = "nav")
-    public static String active(String prefix, Long id, String currentPath) {
-        return active(prefix + id, currentPath);
+    public static String active(String startPath, String currentPath) {
+        if (Objects.equals(startPath, "/")) {
+            return (Objects.equals(currentPath, "/")) ? BUTTON_STYLE_ACTIVE : BUTTON_STYLE_INACTIVE;
+        }
+        if (startPath != null && currentPath.startsWith(startPath)) {
+            return BUTTON_STYLE_ACTIVE;
+        }
+        return BUTTON_STYLE_INACTIVE;
     }
 }
